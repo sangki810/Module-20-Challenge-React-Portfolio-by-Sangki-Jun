39df15953c40b7cf6aaeafee37f6870f3988aaad
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-12-11 17:06:54 -0500

    restructured files and folders in pages, updated App.js and Nav folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Header from './components/pages/Header';
+import Nav from './components/pages/Nav'
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
+import Footer from './components/pages/Footer';
 
 function App() {
-  return 
+  const [pageIndex, setPageIndex] = useState(0);
+ 
+  return (
+    <div className="App">
+      <Header />
+      
+      <Nav 
+        showAbout={() => setPageIndex(0)}
+        showPortfolio={() => setPageIndex(1)}
+        showContact={() => setPageIndex(2)}
+        showResume={() => setPageIndex(3)}
+      />
+
+      {pageIndex === 0 ? (
+        <About />
+      ) : pageIndex === 1 ? (
+        <Portfolio />
+      ) : pageIndex === 2 ? (
+        <Contact />
+      ) : pageIndex === 3 ? (
+        <Resume />
+      ) : (
+        <About />
+      )}
+        
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
