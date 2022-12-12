a5c8195b3d636cac7075c99bb560b7e582956cd0
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-12-12 18:49:02 -0500

    finished footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
+const handleGithubRedirect = () => {
+  window.location = 'https://github.com/sangki810'
+}
+
+const handleLinkedInRedirect = () => {
+  window.location = 'https://www.linkedin.com/in/sangki-jun-8a6a5310b/'
+}
+
+const handleTwitterRedirect = () => {
+  window.location = 'https://twitter.com/sangki810'
+}
+
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className='footer'>
+      <div className='d-flex justify-content-center'>
+        <a className='btn text-light p-1' onClick={handleGithubRedirect}>GitHub</a>
+        <span className='pt-1'>&#x2022;</span>
+        <a className='btn text-light p-1' onClick={handleLinkedInRedirect}>LinkedIn</a>
+        <span className='pt-1'>&#x2022;</span>
+        <a className='btn text-light p-1' onClick={handleTwitterRedirect}>Twitter</a>
+      </div>
       <p className="text-center">&copy; All rights reserved by Sangki B. Jun</p>
     </footer>
   );
