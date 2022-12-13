0ff882a6d17f675b3e97e3e3068399c23a4a0724
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-12-13 15:53:35 -0500

    finished resume

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -2,21 +2,33 @@ import React from "react";
 
 const Resume = () => {
   return (
-    <div className='container pt-3'>
-      <h1 className='underline'><u>Resume</u></h1>
+    <div className="container pt-3">
+      <h1 className="underline">
+        <u>Resume</u>
+      </h1>
       <div>
-        <h3>Front-end techonologies</h3>
-        <div>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>JQuery</li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
+        <h3>Front-end technologies</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>JQuery</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>Bulma</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Back-end technologies</h3>
+        <ul>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ul>
       </div>
     </div>
   );
