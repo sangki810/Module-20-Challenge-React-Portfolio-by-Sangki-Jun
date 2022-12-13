c3d4f36eccdeaa72e3dd6704fb5eae2ca9136dd9
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-12-13 10:03:38 -0500

    updated resume

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Resume = () => {
   return (
     <div className='container pt-3'>
-      <h1>Resume</h1>
+      <h1 className='underline'><u>Resume</u></h1>
+      <div>
+        <h3>Front-end techonologies</h3>
+        <div>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
